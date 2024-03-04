389efa50d99d92e5970a41954fccf7cc8614ebff
Punitha <punitha.lakshmi87@gmail.com>
2024-03-04 11:49:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 
-app.listen('3000',() => {
+const server = app.listen('3000',() => {
   console.log(`Node JS Running Server started at ${3000}`);
 })
 
